aa7105d652bcf8c35f8ec14f14e29f3cd33143dd
Lonni Sandbjerg <lonnisandbjerg@Lonnis-MBP.lan>
2018-04-18 11:29:27 +0200

    .

---
+++ bunnyandfox.js
@@ -0,0 +1,82 @@
+$(window).on("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+
+
+
+
+$("#bunny_container").addClass("bunny_container_walk_in");
+$("#bunny_container").addClass("bunny_start_position");
+$("#bunny_sprite").addClass("bunny_paws");
+$("#bunny_container").on("animationend", trin1A);
+
+
+
+$("#fox_container").addClass("fox_container_walk_in");
+$("#fox_container").addClass("fox_start_position");
+$("#fox_sprite").addClass("fox_paws");
+$("#fox_container").on("animationend", trin1A);
+
+
+//start_music
+//bunny_walk_in
+//fox_walk_in
+//sommer music stop
+}
+
+function trin1A() {
+    console.log("trin1A");
+
+
+    $("#fox_container").off("animationend", trin1A);
+
+    $("#fox_container").removeClass("fox_container_walk_in");
+    $("#fox_sprite").removeClass("fox_paws");
+
+    $("#fox_container").addClass("fox_container_stop");
+    $("#bunny_container").addClass("bunny_container_stop");
+
+
+    //scary music/thunder
+    //bunny_walk_stop
+    //fox_walk_stop
+}
+
+function beslutningA() {
+    consol.log("beslutningA");
+
+    document.getElementById("runaway").onclick = doFunction;
+
+    var el = document.getElementById("runaway");
+    if (el.addEventListener)
+        el.addEventListener("click", doFunction, false);
+    else if (el.attachEvent)
+        el.attachEvent('onclick', doFunction);
+
+
+    document.getElementById("fight").onclick = doFunction;
+
+    var el = document.getElementById("fight");
+    if (el.addEventListener)
+        el.addEventListener("click", doFunction, false);
+    else if (el.attachEvent)
+        el.attachEvent('onclick', doFunction);
+
+
+    //buttons A and B are shown
+    //music "theme stab"
+    //bunny_flee
+    //fox_attack
+    //music "theme stab" stop
+}
+
+function trin2A() {
+    consol.log("trin2A");
+
+    //bunny_stop
+    //fox_stop
+    //music sad start
+
+}
